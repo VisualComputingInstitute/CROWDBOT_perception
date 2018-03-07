053523198b43cb984166ee4228342d5c5fcee3d0
Stefan Breuers <breuers@vision.rwth-aachen.de>
2018-03-07 19:59:00 +0100

    about to test trajectories on robot

--- perception/rwth_people_tracker/rwth_pedestrian_trajectories/src/main.cpp
+++ perception/rwth_people_tracker/rwth_pedestrian_trajectories/src/main.cpp
@@ -59,13 +59,14 @@ void callback(const TrackedPersons::ConstPtr &tps)
 }
 
 // Connection callback that unsubscribes from the tracker if no one is subscribed.
-void connectCallback(message_filters::Subscriber<TrackedPersons> &sub_tra){
+void connectCallback(ros::NodeHandle &n, message_filters::Subscriber<TrackedPersons> &sub_tra, string topic, int qs){
     if(!pub_person_trajectories.getNumSubscribers()
     ) {
         ROS_DEBUG("Trajectories: No subscribers. Unsubscribing.");
         sub_tra.unsubscribe();
     } else {
         ROS_DEBUG("Trajectories: New subscribers. Subscribing.");
+        //sub_tra = n.subscribe(topic.c_str(), qs, &callback);
         sub_tra.subscribe();
     }
 }
@@ -87,18 +88,18 @@ int main(int argc, char **argv)
     // while using different parameters.
     ros::NodeHandle private_node_handle_("~");
     private_node_handle_.param("queue_size", queue_size, int(10));
+    private_node_handle_.param("tracked_persons", sub_topic_tracked_persons, string("/rwth_tracker/tracked_persons"));
 
     ROS_DEBUG("pedestrian_trajectories: Queue size for synchronisation is set to: %i", queue_size);
 
     // Create a subscriber.
-    ros::Subscriber sub = n.subscribe("chatter", 1000, callback);
+    //ros::Subscriber sub = n.subscribe("chatter", 1000, callback);
  
     // Set queue size to 1 because generating a queue here will only pile up images and delay the output by the amount of queued images
     // The immediate unsubscribe is necessary to start without subscribing to any topic because message_filters does nor allow to do it another way.
     message_filters::Subscriber<TrackedPersons> subscriber_tracks(n, sub_topic_tracked_persons.c_str(), 1); subscriber_tracks.unsubscribe();
-
-    ros::SubscriberStatusCallback con_cb = boost::bind(&connectCallback,
-                                                       boost::ref(subscriber_tracks));
+    //ros::Subscriber subscriber_tracks;
+    ros::SubscriberStatusCallback con_cb = boost::bind(&connectCallback, boost::ref(n), boost::ref(subscriber_tracks), sub_topic_tracked_persons, queue_size);
 
     // Create a topic publisher
     private_node_handle_.param("person_trajectories", pub_topic_trajectories, string("/rwth_tracker/person_trajectories"));
