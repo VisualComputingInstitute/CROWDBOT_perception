72c3556b3bc20612502322a536a6de934b3bc855
Stefan Breuers <breuers@vision.rwth-aachen.de>
2018-06-19 13:22:15 +0200

    deleted unneccessary cuda files, library fixes in visualization (c++11), small fix in trajectories

--- visualization/spencer_tracking_rviz_plugin/src/detected_persons_display.h
+++ visualization/spencer_tracking_rviz_plugin/src/detected_persons_display.h
@@ -11,12 +11,12 @@ namespace spencer_tracking_rviz_plugin
     /// The visual of a tracked person.
     struct DetectedPersonVisual
     {
-        shared_ptr<Ogre::SceneNode> sceneNode;
+        boost::shared_ptr<Ogre::SceneNode> sceneNode;
 
-        shared_ptr<PersonVisual> personVisual;
-        shared_ptr<TextNode> detectionIdText, confidenceText, modalityText;
-        shared_ptr<rviz::Arrow> orientationArrow;
-        shared_ptr<CovarianceVisual> covarianceVisual;
+        boost::shared_ptr<PersonVisual> personVisual;
+        boost::shared_ptr<TextNode> detectionIdText, confidenceText, modalityText;
+        boost::shared_ptr<rviz::Arrow> orientationArrow;
+        boost::shared_ptr<CovarianceVisual> covarianceVisual;
 
         float confidence;
         bool hasValidOrientation;
@@ -62,7 +62,7 @@ namespace spencer_tracking_rviz_plugin
         void processMessage(const frame_msgs::DetectedPersons::ConstPtr& msg);
        
         // All currently active tracks, with unique track ID as map key
-        vector<shared_ptr<DetectedPersonVisual> > m_previousDetections;
+        vector<boost::shared_ptr<DetectedPersonVisual> > m_previousDetections;
 
         // Properties
         rviz::BoolProperty* m_render_covariances_property;
