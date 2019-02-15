4bb29563dabb57a007a88f25a2bd9e28e04b217e
qianneng <neng.qian@rwth-aachen.de>
2019-02-15 17:56:08 +0100

    interege the tensorRTyolo code

--- yoloconvertor/src/main_tensorrtyolo.cpp
+++ yoloconvertor/src/main_tensorrtyolo.cpp
@@ -22,11 +22,185 @@
 #include "Vector.h"
 #include "Visual.h"
 
+#include "TrtNet.h"
+#include "argsParser.h"
+#include "configs.h"
+#include "YoloLayer.h"
+#include "dataReader.h"
+#include "eval.h"
 
 using namespace darknet_ros_msgs;
 using namespace message_filters;
+using namespace std;
+using namespace argsParser;
+using namespace Tn;
+using namespace Yolo;
 
 ros::Publisher pub_boundingboxes;
+
+vector<float> prepareImage(cv::Mat& img)
+{
+    using namespace cv;
+
+    int c = parser::getIntValue("C");
+    int h = parser::getIntValue("H");   //net h
+    int w = parser::getIntValue("W");   //net w
+
+    float scale = min(float(w)/img.cols,float(h)/img.rows);
+    auto scaleSize = cv::Size(img.cols * scale,img.rows * scale);
+
+    cv::Mat rgb ;
+    cv::cvtColor(img, rgb, CV_BGR2RGB);
+    cv::Mat resized;
+    cv::resize(rgb, resized,scaleSize,0,0,INTER_CUBIC);
+
+    cv::Mat cropped(h, w,CV_8UC3, 127);
+    Rect rect((w- scaleSize.width)/2, (h-scaleSize.height)/2, scaleSize.width,scaleSize.height);
+    resized.copyTo(cropped(rect));
+
+    cv::Mat img_float;
+    if (c == 3)
+        cropped.convertTo(img_float, CV_32FC3, 1/255.0);
+    else
+        cropped.convertTo(img_float, CV_32FC1 ,1/255.0);
+
+    //HWC TO CHW
+    vector<Mat> input_channels(c);
+    cv::split(img_float, input_channels);
+
+    vector<float> result(h*w*c);
+    auto data = result.data();
+    int channelLength = h * w;
+    for (int i = 0; i < c; ++i) {
+        memcpy(data,input_channels[i].data,channelLength*sizeof(float));
+        data += channelLength;
+    }
+
+    return result;
+}
+
+void DoNms(vector<Detection>& detections,int classes ,float nmsThresh)
+{
+    auto t_start = chrono::high_resolution_clock::now();
+
+    vector<vector<Detection>> resClass;
+    resClass.resize(classes);
+
+    for (const auto& item : detections)
+        resClass[item.classId].push_back(item);
+
+    auto iouCompute = [](float * lbox, float* rbox)
+    {
+        float interBox[] = {
+            max(lbox[0] - lbox[2]/2.f , rbox[0] - rbox[2]/2.f), //left
+            min(lbox[0] + lbox[2]/2.f , rbox[0] + rbox[2]/2.f), //right
+            max(lbox[1] - lbox[3]/2.f , rbox[1] - rbox[3]/2.f), //top
+            min(lbox[1] + lbox[3]/2.f , rbox[1] + rbox[3]/2.f), //bottom
+        };
+
+        if(interBox[2] > interBox[3] || interBox[0] > interBox[1])
+            return 0.0f;
+
+        float interBoxS =(interBox[1]-interBox[0])*(interBox[3]-interBox[2]);
+        return interBoxS/(lbox[2]*lbox[3] + rbox[2]*rbox[3] -interBoxS);
+    };
+
+    vector<Detection> result;
+    for (int i = 0;i<classes;++i)
+    {
+        auto& dets =resClass[i];
+        if(dets.size() == 0)
+            continue;
+
+        sort(dets.begin(),dets.end(),[=](const Detection& left,const Detection& right){
+            return left.prob > right.prob;
+        });
+
+        for (unsigned int m = 0;m < dets.size() ; ++m)
+        {
+            auto& item = dets[m];
+            result.push_back(item);
+            for(unsigned int n = m + 1;n < dets.size() ; ++n)
+            {
+                if (iouCompute(item.bbox,dets[n].bbox) > nmsThresh)
+                {
+                    dets.erase(dets.begin()+n);
+                    --n;
+                }
+            }
+        }
+    }
+
+    //swap(detections,result);
+    detections = move(result);
+
+    auto t_end = chrono::high_resolution_clock::now();
+    float total = chrono::duration<float, milli>(t_end - t_start).count();
+    cout << "Time taken for nms is " << total << " ms." << endl;
+}
+
+
+vector<Bbox> postProcessImg(cv::Mat& img,vector<Detection>& detections,int classes)
+{
+    using namespace cv;
+
+    int h = parser::getIntValue("H");   //net h
+    int w = parser::getIntValue("W");   //net w
+
+    //scale bbox to img
+    int width = img.cols;
+    int height = img.rows;
+    float scale = min(float(w)/width,float(h)/height);
+    float scaleSize[] = {width * scale,height * scale};
+
+    //correct box
+    for (auto& item : detections)
+    {
+        auto& bbox = item.bbox;
+        bbox[0] = (bbox[0] * w - (w - scaleSize[0])/2.f) / scaleSize[0];
+        bbox[1] = (bbox[1] * h - (h - scaleSize[1])/2.f) / scaleSize[1];
+        bbox[2] /= scaleSize[0];
+        bbox[3] /= scaleSize[1];
+    }
+
+    //nms
+    float nmsThresh = parser::getFloatValue("nms");
+    if(nmsThresh > 0)
+        DoNms(detections,classes,nmsThresh);
+
+    vector<Bbox> boxes;
+    for(const auto& item : detections)
+    {
+        auto& b = item.bbox;
+        Bbox bbox =
+        {
+            item.classId,   //classId
+            max(int((b[0]-b[2]/2.)*width),0), //left
+            min(int((b[0]+b[2]/2.)*width),width), //right
+            max(int((b[1]-b[3]/2.)*height),0), //top
+            min(int((b[1]+b[3]/2.)*height),height), //bot
+            item.prob       //score
+        };
+        boxes.push_back(bbox);
+    }
+
+    return boxes;
+}
+
+vector<string> split(const string& str, char delim)
+{
+    stringstream ss(str);
+    string token;
+    vector<string> container;
+    while (getline(ss, token, delim)) {
+        container.push_back(token);
+    }
+
+    return container;
+}
+
+
+
 
 
 //void connectCallback(ros::Subscriber &sub_msg,
@@ -43,10 +217,51 @@ ros::Publisher pub_boundingboxes;
 //        it.subscribe();
 //            }
 //}
+trtNet* net_ptr;
+int outputCount = net.getOutputSize()/sizeof(float);
+unique_ptr<float[]> outputData;
 
 void Callback(const sensor_msgs::ImageConstPtr& img)
 {
     ROS_INFO("get image");
+    outputData = unique_ptr<float[]>(new float[outputCount]);
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    cv::Mat cvmat = cv_ptr->image;
+    vector<float> inputData = prepareImage(cvmat);
+    net_ptr->doInference(inputData.data(), outputData.get());
+
+    //Get Output
+    auto output = outputData.get();
+
+    //first detect count
+    int count = output[0];
+    //later detect result
+    vector<Detection> result;
+    result.resize(count);
+    memcpy(result.data(), &output[1], count*sizeof(Detection));
+
+    auto boxes = postProcessImg(cvmat,result,classNum);  // Ithink here we get boxes.
+
+
+        for(const auto& item : boxes)
+        {
+            cv::rectangle(cvmat,cv::Point(item.left,item.top),cv::Point(item.right,item.bot),cv::Scalar(0,0,255),3,8,0);
+            cout << "class=" << item.classId << " prob=" << item.score*100 << endl;
+            cout << "left=" << item.left << " right=" << item.right << " top=" << item.top << " bot=" << item.bot << endl;
+        }
+        cv::imwrite("result.jpg",cvmat);
+        cv::imshow("result",cvmat);
+        cv::waitKey(25);
+
 }
 
 
@@ -61,7 +276,6 @@ int main(int argc, char **argv)
     string image_topic;
     string boundingboxes;
 
-    //debug
 
 
     // Initialize node parameters from launch file or command line.
@@ -106,7 +320,10 @@ int main(int argc, char **argv)
     private_node_handle_.param("bounding_boxs", boundingboxes, string("/bounding_boxs"));
     pub_boundingboxes = n.advertise<darknet_ros_msgs::BoundingBoxes>(boundingboxes, 10);/* con_cb, con_cb)*/;
 
-
+    //build engine
+    string saveName = "yolov3_fp16.engine";
+    net_ptr = new net(saveName);
+    outputCount = net->getOutputSize()/sizeof(float);
 
     ros::spin();
 
