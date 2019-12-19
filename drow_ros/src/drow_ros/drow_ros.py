4ad4234b6501d0e3a44aae56e262bf34362ce136
Dan Jia <dan.jia@rwth-aachen.de>
2019-12-19 23:53:19 +0100

    Minor.
    Former-commit-id: 73e9517f96f7c62132da2a645b765213c4fce499

--- drow_ros/src/drow_ros/drow_ros.py
+++ drow_ros/src/drow_ros/drow_ros.py
@@ -91,6 +91,7 @@ class DrowRos():
             dp.detection_id = self._detection_id
             self._detection_id += 20  # @TODO
             dp.height = 1.85  # @TODO
+            dp.warp_loss = 0.0
             dps.detections.append(dp)
 
         self._dets_pub.publish(dps)
