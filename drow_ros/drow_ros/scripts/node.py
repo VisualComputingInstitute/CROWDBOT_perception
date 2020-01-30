24a936df5fb458b51d26338d85063723c074e1c7
Dan Jia <dan.jia@rwth-aachen.de>
2020-01-30 16:48:15 +0100

    Clean up drow_ros
    Former-commit-id: eba664c27960caa6b9b9c8f900336484357ef29b

--- drow_ros/drow_ros/scripts/node.py
+++ drow_ros/drow_ros/scripts/node.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 import rospy
-from drow_ros.drow_ros import DrowRos
+from drow_ros.drow_ros import DROWRos
 
 
 if __name__ == '__main__':
     rospy.init_node('drow_ros')
     try:
-        DrowRos()
+        DROWRos()
     except rospy.ROSInterruptException:
         pass
     rospy.spin()
--- drow_ros/drow_ros/src/drow_ros/drow_ros.py
+++ drow_ros/drow_ros/src/drow_ros/drow_ros.py
@@ -1,31 +1,35 @@
 import numpy as np
-
 import rospy
 from sensor_msgs.msg import LaserScan
-from visualization_msgs.msg import Marker, MarkerArray
-from geometry_msgs.msg import Point
-
 from frame_msgs.msg import DetectedPerson, DetectedPersons
-
-import utils
-from drow import prediction_to_detection, Detector
-
+from drow.drow_detector import DROWDetector
 # import time
 
+def read_subscriber_param(name):
+    """
+    @brief      Convenience function to read subscriber parameter.
+    """
+    topic = rospy.get_param("~subscriber/" + name + "/topic")
+    queue_size = rospy.get_param("~subscriber/" + name + "/queue_size")
+    return topic, queue_size
 
-def people_filter(xs, ys, confs, thres=0.0):
-    labels = np.argmax(confs, axis=1)
-    is_people = np.logical_and(labels == 3, confs[:, 3] > thres)
-    return xs[is_people], ys[is_people], confs[is_people]
+
+def read_publisher_param(name):
+    """
+    @brief      Convenience function to read publisher parameter.
+    """
+    topic = rospy.get_param("~publisher/" + name + "/topic")
+    queue_size = rospy.get_param("~publisher/" + name + "/queue_size")
+    latch = rospy.get_param("~publisher/" + name + "/latch")
+    return topic, queue_size, latch
 
 
-class DrowRos():
+class DROWRos():
     """ROS node to detect pedestrian using DROW."""
-
     def __init__(self):
         self._detection_id = 0
         self._read_params()
-        self._net = Detector(self.weight_file)
+        self._drow = DROWDetector(self.weight_file)
         self._init()
 
     def _read_params(self):
@@ -33,50 +37,66 @@ class DrowRos():
         @brief      Reads parameters from ROS server.
         """
         self.weight_file = rospy.get_param("~weight_file")
+        self.conf_thresh = rospy.get_param("~conf_thresh")
 
     def _init(self):
         """
         @brief      Initialize ROS connection.
         """
         # Publisher
-        topic, queue_size, latch = utils.read_publisher_param("visual_marker")
-        self._visual_pub = rospy.Publisher(
-                topic, MarkerArray, queue_size=queue_size, latch=latch)
-
-        topic, queue_size, latch = utils.read_publisher_param("detections")
+        topic, queue_size, latch = read_publisher_param("detections")
         self._dets_pub = rospy.Publisher(
                 topic, DetectedPersons, queue_size=queue_size, latch=latch)
 
         # Subscriber
-        topic, queue_size = utils.read_subscriber_param("scan")
+        topic, queue_size = read_subscriber_param("scan")
         self._scan_sub = rospy.Subscriber(
                 topic, LaserScan, self._scan_callback, queue_size=queue_size)
 
     def _scan_callback(self, msg):
-        if not self._net.initialized():
-            self._net.init(msg.angle_min, msg.angle_max, len(msg.ranges))
-
         if self._dets_pub.get_num_connections() == 0:
             return
 
-        scan = np.array(msg.ranges)
+        if not self._drow.laser_spec_set():
+            self._drow.set_laser_spec(angle_inc=msg.angle_increment,
+                                      num_pts=len(msg.ranges))
 
-        # Inference
         # t = time.time()
-        xs_pred, ys_pred, confs_pred = self._net(scan)
-        xs_det, ys_det, confs_det = prediction_to_detection(xs_pred, ys_pred, confs_pred)
-        xs_det, ys_det, confs_det = people_filter(xs_det, ys_det, confs_det)
+        scan = np.array(msg.ranges)
+        dets_xs, dets_ys, dets_cls = self._drow(scan)
         # print(t - time.time())
 
+        # only keep pedestrian
+        dets_cls_label = np.argmax(dets_cls[:, 1:], axis=1) + 1
+        dets_cls_conf = np.max(dets_cls[:, 1:], axis=1)
+        pedestrian_mask = dets_cls_label == 3  # 1 - wheelchair, 2 - people with walking aids, 3 - people
+        dets_xs = dets_xs[pedestrian_mask]
+        dets_ys = dets_ys[pedestrian_mask]
+        dets_cls_conf = dets_cls_conf[pedestrian_mask]
+
+        # confidence threshold
+        conf_mask = dets_cls_conf >= self.conf_thresh
+        dets_xs = dets_xs[conf_mask]
+        dets_ys = dets_ys[conf_mask]
+        dets_cls_conf = dets_cls_conf[conf_mask]
+
+        # convert and publish ros msg
+        dps_msg = self._detections_to_ros_msg(dets_xs, dets_ys, dets_cls_conf)
+        dps_msg.header = msg.header
+        self._dets_pub.publish(dps_msg)
+
+
+    def _detections_to_ros_msg(self, dets_xs, dets_ys, dets_conf):
         dps = DetectedPersons()
-        dps.header = msg.header
-        for x, y, conf in zip(xs_det, ys_det, confs_det):
+        for x, y, conf in zip(dets_xs, dets_ys, dets_conf):
             dp = DetectedPerson()
             dp.modality = DetectedPerson.MODALITY_GENERIC_LASER_2D
-            dp.confidence = conf[-1]
+            dp.confidence = conf
+            # If laser is facing front, DROW's y-axis aligns with the laser
+            # center ray, x-axis points to right
             dp.pose.pose.position.x = y
             dp.pose.pose.position.y = -x
-            dp.pose.pose.position.z = 0.7
+            dp.pose.pose.position.z = 1.0
             dp.pose.pose.orientation.w = 1.0
             # These covariance values are not used for the moment, just
             # place-holder.
@@ -88,50 +108,8 @@ class DrowRos():
             dp.pose.covariance = covar
             dp.bbox_x, dp.bbox_y = 0.0, 0.0
             dp.bbox_w, dp.bbox_h = 0.0, 0.0
+            dp.height = 1.85
+            dp.warp_loss = 0.5  # 0.5 is neutral
             dp.detection_id = self._detection_id
-            self._detection_id += 1  # @TODO
-            dp.height = 1.85  # @TODO
-            dp.warp_loss = 0.5
             dps.detections.append(dp)
-
-        self._dets_pub.publish(dps)
-
-        # RViz
-        if self._visual_pub.get_num_connections() > 0:
-            ms_msg = MarkerArray()
-
-            # Prediction
-            m_msg = Marker()
-            m_msg.header = msg.header
-            m_msg.action = Marker.ADD
-            m_msg.type = Marker.POINTS
-            xs_pred, ys_pred, confs_pred = people_filter(xs_pred, ys_pred, confs_pred, thres=0)
-            m_msg.id = 0
-            m_msg.scale.x, m_msg.scale.y, m_msg.scale.z = 0.1, 0.1, 0.1
-            m_msg.color.r, m_msg.color.g, m_msg.color.b = 0.0, 1.0, 0.0
-            m_msg.color.a = 0.5
-            points = []
-            for x, y in zip(xs_pred, ys_pred):
-                p = Point()
-                p.x, p.y, p.z = y, -x, 0.5
-                points.append(p)
-            m_msg.points = points
-            ms_msg.markers.append(m_msg)
-
-            # Detection
-            m_msg = Marker()
-            m_msg.header = msg.header
-            m_msg.action = Marker.ADD
-            m_msg.type = Marker.POINTS
-            m_msg.id = 1
-            m_msg.scale.x, m_msg.scale.y, m_msg.scale.z = 0.25, 0.25, 0.25
-            m_msg.color.r, m_msg.color.g, m_msg.color.b = 1.0, 1.0, 0.0
-            m_msg.color.a = 1.0
-            points = []
-            for x, y in zip(xs_det, ys_det):
-                p = Point()
-                p.x, p.y, p.z = y, -x, 0.5
-                points.append(p)
-            m_msg.points = points
-            ms_msg.markers.append(m_msg)
-            self._visual_pub.publish(ms_msg)
+            self._detection_id += 1
